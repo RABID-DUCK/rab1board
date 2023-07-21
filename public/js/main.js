89dfa49e788993686cd9e307a76ab3fa04caa049
RABID-DUCK <54327392+RABID-DUCK@users.noreply.github.com>
2023-07-22 00:56:20 +0500

    init

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./resources/js/main.js ***!
+  \******************************/
+function openModal() {
+  console.log('da');
+}
+/******/ })()
+;
