e34dbb0ff562ff6e749d90c25577bfba2a678826
Rab1d <onetaphack@gmail.com>
2023-07-31 16:22:34 +0500

    fix Auth

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">CapiBoard</a>
+        <a class="navbar-brand" href="{{route('board.index')}}">CapiBoard</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                 aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                        aria-expanded="false">
@@ -16,16 +16,7 @@
 
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="#">Action</a></li>
-                        <hr>
-                        <li><a class="dropdown-item" href="{{route('backend.index')}}">Admin</a></li>
-                        @if(Auth::check())
-                        <li><a class="dropdown-item" href="{{route('logout')}}">Выйти</a></li>
-                        @else
-                        <li><a class="dropdown-item" href="{{route('login')}}">Войти</a></li>
-                        <li><a class="dropdown-item" href="{{route('register')}}">Зарегистрироваться</a></li>
-                            @endif
                     </ul>
-
                 </li>
 
                 <li class="nav-item dropdown">
@@ -36,10 +27,43 @@
 
                 </li>
             </ul>
-            <form class="d-flex" role="search">
+
+            <form class="d-flex w-50" role="search">
                 <input class="form-control search me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-search" type="submit">Search</button>
             </form>
+
+            <div class="user">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    @if(Auth::check())
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                           aria-expanded="false">
+                            {{Auth::user()->name}}
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <hr>
+                            <li><a class="dropdown-item" href="{{route('backend.index')}}">Admin</a></li>
+                            <li>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                        @else
+                            <li><a class="dropdown-item auth-link" href="{{route('login')}}">Войти/</a></li>
+                            <li><a class="dropdown-item auth-link" href="{{route('register')}}">Зарегистрироваться</a></li>
+                        @endif
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,31 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/', function (){
+    return view('main.main');
+});
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function(){
-    Route::get('/login', function (){
-        return view('auth.login');
-    })->name('login');
 
-    Route::get('/register', function (){
-        return view('auth.register');
-    })->name('register');
-    Route::get('/logout', function (){
-        return view('main.main');
-    })->name('logout');
+Route::group(['namespace' => 'frontend'], function (){
+    Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('board.index');
+});
 
-    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('board.index');
+Auth::routes();
 
-    Route::group(['prefix' => 'backend'], function (){
-        Route::get('/admin', '\App\Http\Controllers\MainController@index')->name('backend.index');
+    Route::group(['namespace' => 'backend'], function (){
+        Route::get('/admin', [\App\Http\Controllers\MainController::class, 'index'])->name('backend.index');
 
         Route::get('/langs', '\App\Http\Controllers\LangsController@index')->name('backend.langs.index');
         Route::post('/langs/create', '\App\Http\Controllers\LangsController@store')->name('backend.langs.create');
         Route::delete('/langs/delete/{id}', '\App\Http\Controllers\LangsController@delete')->name('backend.langs.delete');
         Route::patch('/langs/update', '\App\Http\Controllers\LangsController@update')->name('backend.langs.update');
     });
-});
 
