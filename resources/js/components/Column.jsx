3261cb725885c8ac21b13a5dd6ce2c437bf2a5e0
kayoshi03 <106015085+kayoshi03@users.noreply.github.com>
2023-08-30 13:46:32 +0500

    wait

--- resources/js/components/Column.jsx
+++ resources/js/components/Column.jsx
@@ -1,22 +1,21 @@
 import { useEffect } from "react"
 import api from "../API/Api"
-import TestDesk from "./TestDesk"
 
 const Column = ({column, id}) => {
-    // const fetch = async() => {
-    //     const desk = await api.post('/desk',  {
-    //         dashboard_id: parseInt(id)
-    //     })
-    //     console.log(desk.data.desk[column.id]);
-    // }
-    // useEffect(() => {
-    //     fetch()
-    // })
+    const fetch = async() => {
+        const desk = await api.post('/desk',  {
+            dashboard_id: parseInt(id)
+        })
+        console.log(desk.data);
+    }
+    useEffect(() => {
+        fetch()
+    }, [])
     return (
         <>
             {column.id}
             {column.title}
-            <TestDesk id={id} column={column.id}></TestDesk>
+            
             <button>Добавить задачу</button>
         </>
     )
